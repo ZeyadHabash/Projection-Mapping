5cf8a850ea87ba1fb87155a15dc241899a7a40fe
Mariam-Mohamed-Ismail <mariammhmdismail@yahoo.com>
2026-02-11 11:16:06 +0200

    update a script

--- Assets/_Sandbox/ScriptsMariam/ClickToDestroy.cs
+++ Assets/_Sandbox/ScriptsMariam/ClickToDestroy.cs
@@ -2,8 +2,26 @@ using UnityEngine;
 
 public class ClickToDestroy : MonoBehaviour
 {
+    private HandController touchingHand = null;
     void OnMouseDown()
     {
         Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        if (touchingHand != null && touchingHand.IsClosed)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var hand = other.GetComponent<HandController>();
+        if (hand == null) return;
+
+        touchingHand = hand;
     }
 }
