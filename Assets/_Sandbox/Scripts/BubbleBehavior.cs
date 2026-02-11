c651981db7ae41203a54798396003339eb5d7f09
Mariam-Mohamed-Ismail <mariammhmdismail@yahoo.com>
2026-02-11 10:39:37 +0200

    Refactor hand, bubble and tracking logic

--- Assets/_Sandbox/Scripts/BubbleBehavior.cs
+++ Assets/_Sandbox/Scripts/BubbleBehavior.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public enum BubbleType
@@ -7,34 +8,123 @@ public enum BubbleType
     DoubleTap
 }
 
+[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Rigidbody))]
 public class BubbleBehavior : MonoBehaviour
 {
     [SerializeField] private string word;
     [SerializeField] private float yValue;
     [SerializeField] private BubbleType type;
-    [SerializeField] private float radius;
+
+    private HandController rightHand;
+    private HandController leftHand;
+
+    private bool rightTouching;
+    private bool leftTouching;
+
+    private bool wasRightInteracting;
+    private bool wasLeftInteracting;
 
     private int interactions;
 
     public string Word => word;
-    public float YValue => yValue;
     public BubbleType Type => type;
-    public float Radius => radius;
-    public int Interactions => interactions;
 
-    public bool WasRightInteracting { get; set; }
-    public bool WasLeftInteracting { get; set; }
+    public event Action<BubbleBehavior> OnCollected;
 
-    public void Configure(string bubbleWord, float bubbleYValue, BubbleType bubbleType, float bubbleRadius)
+    private void Awake()
+    {
+        var rb = GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+        rb.useGravity = false;
+    }
+
+    public void Configure(string bubbleWord, float bubbleYValue, BubbleType bubbleType)
     {
         word = bubbleWord;
         yValue = bubbleYValue;
         type = bubbleType;
-        radius = bubbleRadius;
     }
 
-    public void RegisterInteraction()
+    public void Initialize(HandController right, HandController left)
     {
-        interactions++;
+        rightHand = right;
+        leftHand = left;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var hand = other.GetComponent<HandController>();
+        if (hand == null) return;
+
+        if (hand == rightHand)
+            rightTouching = true;
+
+        if (hand == leftHand)
+            leftTouching = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        var hand = other.GetComponent<HandController>();
+        if (hand == null) return;
+
+        if (hand == rightHand)
+            rightTouching = false;
+
+        if (hand == leftHand)
+            leftTouching = false;
+    }
+
+    private void Update()
+    {
+        if (rightHand == null && leftHand == null)
+            return;
+
+        bool rightInteracting = rightTouching && rightHand != null && rightHand.IsClosed;
+        bool leftInteracting = leftTouching && leftHand != null && leftHand.IsClosed;
+
+        switch (type)
+        {
+            case BubbleType.Basic:
+                if (rightInteracting || leftInteracting)
+                {
+                    Collect();
+                }
+                break;
+
+            case BubbleType.BothHands:
+                if (rightHand != null && leftHand != null)
+                {
+                    if (rightInteracting && leftInteracting)
+                        Collect();
+                }
+                else if (rightInteracting || leftInteracting)
+                {
+                    Collect();
+                }
+                break;
+
+            case BubbleType.DoubleTap:
+
+                if (rightInteracting && !wasRightInteracting)
+                    interactions++;
+
+                if (leftInteracting && !wasLeftInteracting)
+                    interactions++;
+
+                if (interactions >= 2)
+                    Collect();
+
+                break;
+        }
+
+        wasRightInteracting = rightInteracting;
+        wasLeftInteracting = leftInteracting;
+    }
+
+    private void Collect()
+    {
+        OnCollected?.Invoke(this);
     }
 }
--- Assets/_Sandbox/Scripts/TestTrackingNew.cs
+++ Assets/_Sandbox/Scripts/TestTrackingNew.cs
@@ -4,155 +4,122 @@ using extOSC;
 
 public class TestTrackingNew : MonoBehaviour
 {
-    private OSCReceiver temp;
-    [SerializeField] private MeshRenderer r_rend;
-    [SerializeField] private Transform r_cubeTransform;
-    [SerializeField] private MeshRenderer l_rend;
-    [SerializeField] private Transform l_cubeTransform;
+    private OSCReceiver receiver;
+
+    [Header("Hands")]
+    [SerializeField] private HandController rightHand;
+    [SerializeField] private HandController leftHand;
+
+    [Header("Keyboard Debug")]
     [SerializeField] private bool enableKeyboardMovement = true;
     [SerializeField] private float keyboardMoveSpeed = 5f;
 
-    private float rx;
-    private float ry;
-    private float lx;
-    private float ly;
-
-    private float rightClosedValue;
-    private float leftClosedValue;
-
-    private const float HandZ = -0.5f;
-    private float handYOffset = 4.5f * 2;
-    private static float UNITS_RATIO = 10;
-
-    public Transform RightHandTransform => r_cubeTransform;
-    public Transform LeftHandTransform => l_cubeTransform;
-    public bool RightHandClosed => rightClosedValue > 0.5f;
-    public bool LeftHandClosed => leftClosedValue > 0.5f;
+    public Transform RightHandTransform => rightHand.HandTransform;
+    public Transform LeftHandTransform => leftHand.HandTransform;
+    public bool RightHandClosed => rightHand.IsClosed;
+    public bool LeftHandClosed => leftHand.IsClosed;
 
     void Awake()
     {
-        temp = GetComponent<OSCReceiver>();
+        receiver = GetComponent<OSCReceiver>();
     }
 
     void Start()
     {
-        temp.Bind("/p1/hand_r_closed", TestMethod);
-        temp.Bind("/p1/hand_r:tx", MoveHandRx);
-        temp.Bind("/p1/hand_r:ty", MoveHandRy);
-        temp.Bind("/p1/hand_l_closed", TestLMethod);
-        temp.Bind("/p1/hand_l:tx", MoveHandLx);
-        temp.Bind("/p1/hand_l:ty", MoveHandLy);
-        //p1/hand_r_closed p1/hand_r:tx p1/hand_r:ty
+        receiver.Bind("/p1/hand_r_closed", OnRightClosed);
+        receiver.Bind("/p1/hand_r:tx", OnRightX);
+        receiver.Bind("/p1/hand_r:ty", OnRightY);
+
+        receiver.Bind("/p1/hand_l_closed", OnLeftClosed);
+        receiver.Bind("/p1/hand_l:tx", OnLeftX);
+        receiver.Bind("/p1/hand_l:ty", OnLeftY);
     }
 
     void Update()
     {
         if (!enableKeyboardMovement)
-        {
             return;
-        }
 
         var keyboard = Keyboard.current;
         if (keyboard == null)
-        {
             return;
-        }
 
         ApplyKeyboardClosedStates(keyboard);
 
         var leftInput = new Vector2(
             ReadAxis(keyboard.aKey.isPressed, keyboard.dKey.isPressed),
             ReadAxis(keyboard.sKey.isPressed, keyboard.wKey.isPressed));
+
         var rightInput = new Vector2(
             ReadAxis(keyboard.leftArrowKey.isPressed, keyboard.rightArrowKey.isPressed),
             ReadAxis(keyboard.downArrowKey.isPressed, keyboard.upArrowKey.isPressed));
 
-        if (leftInput.sqrMagnitude >= Mathf.Epsilon || rightInput.sqrMagnitude >= Mathf.Epsilon)
+        if (leftInput.sqrMagnitude >= Mathf.Epsilon ||
+            rightInput.sqrMagnitude >= Mathf.Epsilon)
         {
-            var delta = keyboardMoveSpeed * Time.deltaTime;
-            ApplyKeyboardOffset(leftInput * delta, rightInput * delta);
+            float delta = keyboardMoveSpeed * Time.deltaTime;
+
+            leftHand.ApplyKeyboardDelta(leftInput * delta);
+            rightHand.ApplyKeyboardDelta(rightInput * delta);
         }
     }
 
-    void TestMethod(OSCMessage message)
+    #region OSC Forwarding
+
+    void OnRightClosed(OSCMessage message)
     {
-        var temp = message.Values[0].FloatValue;
-        Debug.Log($"--- blah {temp}");
-        rightClosedValue = temp;
-        SetHandState(r_rend, r_cubeTransform, temp, rx, ry);
+        float value = message.Values[0].FloatValue;
+        Debug.Log($"--- right closed {value}");
+        rightHand.SetClosed(value);
     }
 
-    void MoveHandRx(OSCMessage message)
+    void OnRightX(OSCMessage message)
     {
-        rx = message.Values[0].FloatValue * UNITS_RATIO;
+        rightHand.SetRawX(message.Values[0].FloatValue);
     }
 
-    void MoveHandRy(OSCMessage message)
+    void OnRightY(OSCMessage message)
     {
-        ry = message.Values[0].FloatValue * UNITS_RATIO + handYOffset;
+        rightHand.SetRawY(message.Values[0].FloatValue);
     }
 
-    void TestLMethod(OSCMessage message)
+    void OnLeftClosed(OSCMessage message)
     {
-        var temp = message.Values[0].FloatValue;
-        Debug.Log($"--- blah {temp}");
-        leftClosedValue = temp;
-        SetHandState(l_rend, l_cubeTransform, temp, lx, ly);
+        float value = message.Values[0].FloatValue;
+        Debug.Log($"--- left closed {value}");
+        leftHand.SetClosed(value);
     }
 
-    void MoveHandLx(OSCMessage message)
+    void OnLeftX(OSCMessage message)
     {
-        lx = message.Values[0].FloatValue * UNITS_RATIO;
+        leftHand.SetRawX(message.Values[0].FloatValue);
     }
 
-    void MoveHandLy(OSCMessage message)
+    void OnLeftY(OSCMessage message)
     {
-        ly = message.Values[0].FloatValue * UNITS_RATIO + handYOffset;
+        leftHand.SetRawY(message.Values[0].FloatValue);
     }
 
-    private void SetHandState(MeshRenderer renderer, Transform cubeTransform, float closedValue, float x, float y)
-    {
-        renderer.material.color = closedValue > 0 ? Color.red : Color.cyan;
-        cubeTransform.position = new Vector3(x, y, HandZ);
-    }
+    #endregion
 
-    private void ApplyKeyboardOffset(Vector2 leftDelta, Vector2 rightDelta)
-    {
-        lx += leftDelta.x;
-        ly += leftDelta.y;
-        rx += rightDelta.x;
-        ry += rightDelta.y;
-
-        if (r_cubeTransform != null)
-        {
-            r_cubeTransform.position = new Vector3(rx, ry, HandZ);
-        }
-
-        if (l_cubeTransform != null)
-        {
-            l_cubeTransform.position = new Vector3(lx, ly, HandZ);
-        }
-    }
+    #region Keyboard
 
     private void ApplyKeyboardClosedStates(Keyboard keyboard)
     {
-        var rightClosed = keyboard.rightShiftKey.isPressed ? 1f : 0f;
-        var leftClosed = keyboard.leftShiftKey.isPressed ? 1f : 0f;
+        bool rightClosed = keyboard.rightShiftKey.isPressed;
+        bool leftClosed = keyboard.leftShiftKey.isPressed;
 
-        rightClosedValue = rightClosed;
-        leftClosedValue = leftClosed;
-
-        SetHandState(r_rend, r_cubeTransform, rightClosed, rx, ry);
-        SetHandState(l_rend, l_cubeTransform, leftClosed, lx, ly);
+        rightHand.SetKeyboardClosed(rightClosed);
+        leftHand.SetKeyboardClosed(leftClosed);
     }
 
     private static float ReadAxis(bool negative, bool positive)
     {
         if (negative == positive)
-        {
             return 0f;
-        }
 
         return positive ? 1f : -1f;
     }
+
+    #endregion
 }
