8778d2c0a6620d3b74cfbadffbbbd6f03b133bb3
Mariam-Mohamed-Ismail <mariammhmdismail@yahoo.com>
2026-02-12 18:58:08 +0200

    look at target

--- Assets/_Sandbox/ScriptsMariam/EnemyMove.cs
+++ Assets/_Sandbox/ScriptsMariam/EnemyMove.cs
@@ -5,7 +5,11 @@ public class EnemyMove : MonoBehaviour
     [Header("Speed Settings")]
     public float speed = 2f;            
     public float acceleration = 0.5f;   
-    public float maxSpeed = 5f;         
+    public float maxSpeed = 5f;
+
+
+    [Header("Rotation Settings")]
+    public float rotationSpeed = 5f;
 
     private Transform player;
 
@@ -26,9 +30,12 @@ public class EnemyMove : MonoBehaviour
                 speed = maxSpeed;
         }
 
-        
+        transform.LookAt(player);
+
         Vector3 target = new Vector3(player.position.x, player.position.y, transform.position.z);
         Vector3 dir = (target - transform.position).normalized;
         transform.position += dir * speed * Time.deltaTime;
+
+       
     }
 }
